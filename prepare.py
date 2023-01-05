0a7143dffc455aea98bf32b03ea04fbe211fcdcc
Dev Gurung <dev.gurung141@gmail.com>
2023-01-05 11:57:13 -0600

    added updated version

--- prepare.py
+++ prepare.py
@@ -15,7 +15,6 @@ def prep_telco(df):
     '''
     This function takes in a dataframe and makes changes in a dataframe and return clean dataframe
     '''
-    
     # drop unnecessary columns    
     df.drop(columns = ['payment_type_id', 'contract_type_id', 'internet_service_type_id' ] , inplace=True)
     
@@ -23,19 +22,20 @@ def prep_telco(df):
     df.total_charges= df.total_charges.str.replace(' ','0').astype('float64')
     
     # creae a list of categorical variables
-    cat_cols = ['gender','partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing', 'churn','internet_service_type','contract_type','payment_type']
+    cat_cols_1 = ['gender','partner','dependents','phone_service', 'paperless_billing', 'churn']
+    cat_cols_2= ['multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','internet_service_type','contract_type','payment_type']
     
     # create dummy variable 
-    dummies = pd.get_dummies(df[cat_cols],drop_first=True)
+    dummies_1 = pd.get_dummies(df[cat_cols_1],drop_first=True)
+    dummies_2 = pd.get_dummies(df[cat_cols_2])
     
     # concate dummy varibles with telco
-    df = pd.concat([df, dummies],axis=1)
+    df = pd.concat([df, dummies_1, dummies_2],axis=1)
     
     #convert column names to lower case
     df.columns= df.columns.str.lower()
     
     return df
-
 
 
 def train_validate_test_split(df, target, seed=42):
